cf4a3745f880850fb8deb1739e5c6b8932b66858
Daenyth <Daenyth+git@gmail.com>
2011-12-10 11:35:36 -0500

    conflict.py: Add properties correctly

--- src/scripter/conflict.py
+++ src/scripter/conflict.py
@@ -6,40 +6,26 @@ class Conflict(db.Model):
     """
     A Burning Wheel scripted conflict
     """
-    def __init__(self, character_count):
-        """
-        Initialize a conflict
-
-        character_count: The number of characters involved in the conflict
-        """
-        self.exchanges = db.ListProperty(Exchange, [db.ReferenceProperty(Exchange)])
-        self.characters = db.ListProperty(Character)
-        for char in xrange(character_count):
-            self.characters.append(db.ReferenceProperty(Character))
 
 class Exchange(db.Model):
     """
     One exchange in a scripted conflict
     """
-    def __init__(self):
-        """
-        Initialize an exchange
-        """
-        volley = db.ReferenceProperty(Volley)
-        self.volleys = db.ListProperty(Volley, [volley, volley, volley])
-        self.ready = db.BooleanProperty(False)
+    conflict = db.ReferenceProperty(Conflict, collection_name="exchanges")
+    ready = db.BooleanProperty(False)
 
 class Volley(db.Model):
     """
     One volley in an exchange
     """
-    pass
+    volley_number = db.IntegerProperty
+    exchange = db.ReferenceProperty(Exchange, collection_name="volleys")
 
 class Character(db.Model):
     """
     One character involved in a scripted conflict
     """
-    def __init__(self, name=None):
-        self.name = db.StringProperty(name)
-        self.password = db.StringProperty()
-        self.finalized = db.BooleanProperty(False)
+    conflict = db.ReferenceProperty(Conflict, collection_name="characters")
+    name = db.StringProperty()
+    password = db.StringProperty()
+    finalized = db.BooleanProperty(False)
